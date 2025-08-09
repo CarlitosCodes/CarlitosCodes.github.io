e0cff0ac1986b8e41027ed725b83d1698748435d
carlosv <102779841+CarlitosCodes@users.noreply.github.com>
2025-08-09 12:57:37 -0700

    added an image second try

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -1,9 +1,19 @@
 // src/components/Placeholder.jsx
+
+import hero from "../assets/hero.jpg";
+
 export default function Placeholder() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <img
+          src={hero}
+          alt="working-progress image"
+          className="mx-auto w-full max-w-md rounded-2xl shadow-lg"
+          loading="eager"
+          decoding="async"
+        />
       <h1 className="text-4xl font-bold text-gray-800">
-        Hello, World! 🌍 This is my spanking new website
+        Hello, World! 🌍 This is Carlos Website.
       </h1>
     </div>
   );
