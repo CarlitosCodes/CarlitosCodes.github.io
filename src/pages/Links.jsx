2848617b84461128283f648f1bd6f3240e9a7e71
Carlos <102779841+CarlitosCodes@users.noreply.github.com>
2025-09-01 07:00:16 -0700

    updated about

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -4,7 +4,24 @@ export default function Links() {
       <section className="max-w-3xl mx-auto space-y-6 text-left">
         <h2 className="text-2xl font-bold !text-left">Hi! 👋️</h2>
         <p className="text-gray-900 dark:text-gray-300 !text-left">
-          I'm Carlos, a Controls Engineer doing my Masters in Computer Science with a dream of working in ML.
+          I'm Carlos! I am a Controls Engineer doing a Masters in Computer Science with a dream of working in ML or AI (I'm still exploring).
+
+          Currently currently working as a data/controls engineer at the happiest place on earth.
+          I work with all things IIoT which led me to explore and become passionate about ML.
+
+          Graduated with a BS Electrical Engineering with focus in electronics from Cal Poly Pomona.
+
+          Currently, I am: 
+
+          - Exploring AI agents, starting with a simple personalized chatbot to a more complex calender assistant. 
+          - Learning about website (This is my first Website even)
+          - Leaning some of the tools mentioned in most ML/AI job description and incorporate them on my projects. 
+
+        Outside of work I like the beach, exercise, and being dad. 
+        
+        Find out about my progress on my blog. 
+        See my portfolio section and find me on LinkedIn and shoot me a DM.
+
         </p>
       </section>
     </main>
