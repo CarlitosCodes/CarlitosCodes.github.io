ba5163645c8f93f26af6056d60c95c6b9e391172
Carlos <102779841+CarlitosCodes@users.noreply.github.com>
2025-09-12 06:01:50 -0700

    about page updated

--- src/pages/Links.jsx
+++ src/pages/Links.jsx
@@ -3,24 +3,34 @@ export default function Links() {
     <main className="max-w-5xl mx-auto px-4 py-6">
       <section className="max-w-3xl mx-auto space-y-6 text-left">
         <h2 className="text-2xl font-bold !text-left">Hi! 👋️</h2>
-        <p className="text-gray-900 dark:text-gray-300 !text-left">
+<p className="text-gray-900 dark:text-gray-300 !text-left">
           I'm Carlos! I am a Controls Engineer doing a Masters in Computer Science with a dream of working in ML or AI (I'm still exploring).
-
-          Currently currently working as a data/controls engineer at the happiest place on earth.
-          I work with all things IIoT which led me to explore and become passionate about ML.
-
-          Graduated with a BS Electrical Engineering with focus in electronics from Cal Poly Pomona.
-
-          Currently, I am: 
-
-          - Exploring AI agents, starting with a simple personalized chatbot to a more complex calender assistant. 
-          - Learning about website (This is my first Website even)
-          - Leaning some of the tools mentioned in most ML/AI job description and incorporate them on my projects. 
-
-        Outside of work I like the beach, exercise, and being dad. 
+        </p>
         
-        Find out about my progress on my blog. 
-        See my portfolio section and find me on LinkedIn and shoot me a DM.
+        <p className="text-gray-900 dark:text-gray-300 !text-left">
+          Currently working as a data/controls engineer at the happiest place on earth. I work with all things IIoT which led me to explore and become passionate about ML.
+        </p>
+        
+        <p className="text-gray-900 dark:text-gray-300 !text-left">
+          Graduated with a BS Electrical Engineering with focus in electronics from Cal Poly Pomona.
+        </p>
+        
+        <p className="text-gray-900 dark:text-gray-300 !text-left">
+          Currently, I am:
+        </p>
+        <ul className="list-disc pl-6 text-gray-900 dark:text-gray-300">
+          <li>Exploring AI agents, starting with a simple personalized chatbot to a more complex calendar assistant.</li>
+          <li>Learning about websites (This is my first website ever).</li>
+          <li>Learning some of the tools mentioned in most ML/AI job descriptions and incorporating them into my projects.</li>
+        </ul>
+        
+        <p className="text-gray-900 dark:text-gray-300 !text-left">
+          Outside of work I like the beach, exercise, and being a dad.
+        </p>
+        
+        <p className="text-gray-900 dark:text-gray-300 !text-left">
+          Find out about my progress on my blog.<br />
+          See my portfolio section and find me on LinkedIn and shoot me a DM.
         </p>
       </section>
     </main>
